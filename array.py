d84e9b51db89b33327978a8b3c84565020e3cb6a
Huan Nguyen <nguyenhuands99@gmail.com>
2020-02-08 00:47:54 +0700

    Add array.h
    solve some common problems: find second smallest elememt,
    first non-repeating character in string :v
    
    [Ticket: X]

---
+++ array.py
@@ -0,0 +1,35 @@
+import sys
+
+#### Second minimum element in array
+# elements = [10,31,20,100,2]
+# first = second = sys.maxsize
+
+# for i in range(0,len(elements)):
+# 	if elements[i]<first:
+# 		second = first
+# 		first = elements[i]
+# 	elif (elements[i]<second) and (elements[i] != first):
+# 		second = elements[i]
+
+
+# print(second)
+
+#### First non repeating in string
+# NO_OF_CHARS = 256
+# sample_string = "geekforgeeks is awesome"
+
+# def getCharCounted(string):
+# 	count = [0] * NO_OF_CHARS
+# 	for i in string:
+# 		count[ord(i)] += 1
+# 	return count
+
+# count = getCharCounted(sample_string)
+
+# for i in sample_string:
+# 	if count[ord(i)] == 1:
+# 		print(i)
+# 		break
+
+
+
